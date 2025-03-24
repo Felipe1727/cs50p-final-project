1f6db17ccbc83a9a99240eee0abd3ed62b33de03
Juan Felipe <155260142+Felipe1727@users.noreply.github.com>
2025-03-23 21:12:16 -0500

    Add Binance API call with basic HTTP response validation

--- project.py
+++ project.py
@@ -1,6 +1,22 @@
+import requests, math
+
 def main():
     ...
 
+def binance_api(url : str, params : dict):
+    return validate_http_response(requests.get(url=url, params=params))
+    
+
+def validate_http_response(response : requests.models.Response):
+    match response.status_code // 10 ** (int(math.log(response.status_code, 10))):
+        case 4:
+            print("User side error")
+            return None
+        case 5:
+            print("Server side error")
+            return None
+        case _:
+            return response
 
 if __name__ == "__main__":
     main()

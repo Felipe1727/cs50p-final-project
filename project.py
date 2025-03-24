39b4d505dc3227ffb77b3279298f29de7e488cb2
Juan Felipe <155260142+Felipe1727@users.noreply.github.com>
2025-03-24 11:26:11 -0500

    Add logging config and basic logging - Configure logging to an external file named basic.log - Implement log messages to validate_http_response and check_error_codes funtions

--- project.py
+++ project.py
@@ -1,7 +1,14 @@
-import requests, math
+import requests, math, logging
+
+logging.basicConfig(
+    filename="basic.log",
+    level=logging.DEBUG,
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
 
 def main():
-    ...
+    binance_api(url="https://api.binance.com/api/v3/ticker/price", params={"wrong" : "wrong"})
 
 def binance_api(url : str, params : dict):
     if response := validate_http_response(requests.get(url=url, params=params)) != None:
@@ -12,10 +19,10 @@ def binance_api(url : str, params : dict):
 def validate_http_response(response : requests.models.Response):
     match response.status_code // 10 ** (int(math.log(response.status_code, 10))):
         case 4:
-            print("User side error")
+            logging.error(f"User side error - HTTP {response.status_code}")
             return None
         case 5:
-            print("Server side error")
+            logging.error(f"Server side error - HTTP {response.status_code}")
             return None
         case _:
             return response
@@ -23,8 +30,9 @@ def validate_http_response(response : requests.models.Response):
 def check_error_codes(response : requests.models.Response):
     r = response.json()
     if "code" in r:
-        print(f"Error code {r["code"]}\n{r["msg"]}")
+        logging.error(f"Error code {r["code"]}: {r["msg"]}")
         return None
+    return response
 
 if __name__ == "__main__":
     main()

85ca7dc488984d63b938e1e97972d2ca1b9be3e8
Juan Felipe <155260142+Felipe1727@users.noreply.github.com>
2025-04-02 14:14:37 -0500

    Add functionality to replace_invalid rows in trending() function - Replaces the rows with invalid data to their equivalent in USDT

--- project.py
+++ project.py
@@ -4,8 +4,9 @@ import requests, math, logging, pandas as pd
 def main():
     ...
 
-def binance_api(endpoint : str, params = {}):
-    response = validate_http_response(requests.get(url="https://api.binance.com/api/v3" + endpoint, params=params))
+
+def binance_api(endpoint : str, params = {}, headers = {}):
+    response = validate_http_response(requests.get(url="https://api.binance.com/api/v3" + endpoint, params=params, headers=headers))
     if response != None:
         return check_error_codes(response= response)
     return response
@@ -29,7 +30,7 @@ def check_error_codes(response : requests.models.Response):
         return None
     return response
 
-def trending():
+def trending(_symbol = "USDC", replace_invalid = False) -> pd.DataFrame:
     response = binance_api("/ticker/24hr")
     
     df = pd.DataFrame(response.json())
@@ -52,11 +53,27 @@ def trending():
         "quoteVolume"
     ]
 
-    df = df[df["symbol"].str.endswith("USDC")]
+    df = df[df["symbol"].str.endswith(_symbol)]
     df[columns_range] = df[columns_range].astype(float)
     df = df.reset_index(drop=True)
+    if replace_invalid:
+        df = replace(df)
     return df
         
+def replace(df : pd.DataFrame):
+    #TO-DO: Migrate the code in main into another function that works along with trending()
+    invalid_usdc = (df[df['count'].astype(float) == 0].index[:])
+    usdt_df = trending("USDT")
+    invalid_usdc = df.loc[invalid_usdc]["symbol"].str.replace("USDC", "USDT")
+    usdt_df = usdt_df[usdt_df["symbol"].isin(invalid_usdc)]
+    #Eliminate the rows of the invalid_usdc_symbols that are not in usdt_df["symbol"]
+    invalid_usdc = invalid_usdc[invalid_usdc.isin(usdt_df["symbol"])].sort_values()
+    usdt_df = usdt_df.sort_values(by="symbol")
+    usdt_df.index = invalid_usdc.index    
+
+    for i in usdt_df.index:
+        df.loc[i] = usdt_df.loc[i]
+    return df
 
 
 if __name__ == "__main__":
